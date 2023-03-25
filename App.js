c86431d3e2f179eb9ad0ad688ee52649cc62bb2f
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-25 21:22:44 +0530

    Updated the code to show the Input Text and Button

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style = {styles.dummyText}>Another Text to Display</Text>
-      <Text style = {styles.dummyText}>Hello New World!!!!!!!</Text>
-      <Button title='Tap Me!' />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Your course goals!!'></TextInput>
+        <Button title='Add Goal'/>
+      </View>
+      <View>
+        <View>
+          <Text>List of Goals</Text>
+        </View>
+      </View>
+      
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  dummyText: {
-    margin:16, 
-    padding:16,
-    borderWidth:2, 
-    borderColor:'blue',
-  },
+  appContainer: {
+    padding:50
+  }
+  
 });
