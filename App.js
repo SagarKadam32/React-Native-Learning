000ee3c0c36c4f5b2d739b70e29c404d0cc9153a
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-25 21:37:05 +0530

    Added the textInput Style

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder='Your course goals!!'></TextInput>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Your course goals!!' style={styles.textInput}></TextInput>
         <Button title='Add Goal'/>
       </View>
       <View>
@@ -21,6 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding:50
-  }
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+
+  },
   
 });
