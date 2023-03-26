5a4b9e42ca9b1679eed965430ba341439f3d5fd6
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-26 20:07:17 +0530

    Added the headerView

--- Projects/Demo_LikeItApp/components/header.js
+++ Projects/Demo_LikeItApp/components/header.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function Header() {
     return (
         <View style={styles.headerView}>
             <Text style={{ flex: 1, fontSize: 24 }}>Like It!</Text>
-            <Text style={{ fontSize: 24 }}>0</Text>
+
+            <FontAwesome name="heart" size={24} color="red" />
+            <Text style={{ fontSize: 24, marginLeft: 5 }}>0</Text>
         </View>
 
     );
@@ -18,7 +21,8 @@ const styles = StyleSheet.create({
         borderBottomColor: 'black',
         backgroundColor: '#D0CEE2',
         paddingHorizontal: 20,
-        paddingVertical: 5
+        paddingVertical: 5,
+        alignItems: 'center'
 
     }
 });
