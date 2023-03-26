f92e4305fac466796e9d4b317ca774abd5b85704
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-26 12:45:06 +0530

    Added the advance level code

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  
+  function goalInputHandler(enteredText) {
+    //console.log(enteredText);
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    console.log(enteredGoalText);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Your course goals!!' style={styles.textInput}></TextInput>
-        <Button title='Add Goal'/>
+        <TextInput  placeholder='Your course goals!!' 
+                    style={styles.textInput}
+                    onChangeText={goalInputHandler}>
+                    
+        </TextInput>
+        <Button title='Add Goal'
+                onPress={addGoalHandler }
+        />
       </View>
       <View>
-        <View>
+        <View style={styles.goalsContainer}>
           <Text>List of Goals</Text>
         </View>
       </View>
@@ -20,19 +39,31 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding:50
+    //flex: 1,
+    paddingTop:50,
+    paddingHorizontal:16
   },
   inputContainer: {
+    //flex: 0.1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    //borderBottomWidth: 1,
+    //borderBottomColor: '#CCCCCC'
+
   },
   textInput:{
     borderWidth: 1,
     borderColor: '#CCCCCC',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8
 
   },
+  goalsContainer:{
+   // flex: 0.5
+   marginTop: 4
+  }
   
 });
