2c24c3f5ed514208a992d605532d40a1fb9bc921
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-26 15:16:12 +0530

    Add File

---
+++ function.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+
+const Avatar = () => {
+    return (<View style={{ height: 50, width: 50, backgroundColor: 'red', borderRadius: 25 }}>
+    </View>);
+};
+
+export default Avatar;
