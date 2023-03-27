f7d32debeaff071644ea2f439a282f5c3fdcfb7c
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-27 13:05:12 +0530

    Added the array for images

--- Projects/Demo_LikeItApp/App.js
+++ Projects/Demo_LikeItApp/App.js
@@ -2,22 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import Header from './components/header';
 import Card from './components/card';
+import React from 'react';
 
 import { FontAwesome } from '@expo/vector-icons';
 
+const data = [
+  { image: require('./assets/pic-1.jpg'), id: 1, liked: false },
+  //{ image: require('./assets/pic-2.jpg'), id: 2, liked: false },
+  { image: require('./assets/pic-3.jpg'), id: 3, liked: false },
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <ScrollView style={styles.body}>
-        <Card image={require('./assets/pic-1.jpg')} liked={true} />
-        <Card image={require('./assets/pic-2.jpg')} liked={false} />
-        <Card image={require('./assets/pic-3.jpg')} liked={false} />
-      </ScrollView>
-      <StatusBar style="auto" />
-    </SafeAreaView >
-  );
+]
+
+class App extends React.Component {
+  state = {}
+
+  onPicLike = () => { }
+
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <ScrollView style={styles.body}>
+          {data.map((item => <Card image={item.image} id={item.id} liked={item.liked} />))}
+
+
+        </ScrollView>
+        <StatusBar style="auto" />
+      </SafeAreaView >
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -31,3 +45,5 @@ const styles = StyleSheet.create({
   }
 
 });
+
+export default App;
