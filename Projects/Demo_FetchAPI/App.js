99d8ea6ef3f65d20f49c3c4479a28ee5f2c4c2c5
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-27 16:48:10 +0530

    Fetch API basic call

--- Projects/Demo_FetchAPI/App.js
+++ Projects/Demo_FetchAPI/App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+
+  componentDidUpdate() {
+    this.fetchRequest();
+  }
+
+  fetchRequest = () => {
+    const api = 'https://api.themoviedb.org/3/movie/popular?api_key=33c2b018c41ac61aa087e57644ba423b&language=en-US&page=1';
+    //const res = fetch(api);
+    fetch(api)
+      .then((response) =>
+        response.json().then((data) => console.log(data)))
+      .catch((err) => { console.log(err) });
+  };
+
+  render() {
+    return (
+      <View style={styles.container} >
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +35,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
