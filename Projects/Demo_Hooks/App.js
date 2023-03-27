85549bb4f67a8d04e9b6b019480b3d26a33ddf47
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-27 20:05:03 +0530

    Added the code logic

--- Projects/Demo_Hooks/App.js
+++ Projects/Demo_Hooks/App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  //const value = 20;
+  const [value, setValue] = useState(20);
+  const [value2, setValue2] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 32, marginBottom: 30 }}>Value : {value}</Text>
+      <TouchableOpacity onPress={() => { setValue(value + 10) }}>
+        <Text>Add 10 to value..</Text>
+      </TouchableOpacity>
+
+      <Text style={{ fontSize: 32, marginBottom: 30 }}>Value : {value2}</Text>
+      <TouchableOpacity onPress={() => { setValue2(value2 + 10) }}>
+        <Text>Add 10 to value..</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
