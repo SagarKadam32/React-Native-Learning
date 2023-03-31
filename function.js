3e9d59eab7de0b68d48ae2a4d9b0af91072c6cb7
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-03-31 18:50:49 +0530

    Removed the RN Course Folder & Files

--- function.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-
-const Avatar = () => {
-    return (<View style={{ height: 50, width: 50, backgroundColor: 'red', borderRadius: 25 }}>
-    </View>);
-};
-
-export default Avatar;
